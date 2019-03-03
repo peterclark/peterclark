d0ad583c0b92cbb6b70edf2f896056702c4a14fb
Peter Clark <peter@5clarks.net>
2019-03-03 15:25:27 -0600

    shorten title

--- src/components/usc.js
+++ src/components/usc.js
@@ -7,20 +7,21 @@ const Usc = () => {
       <article className="col-sm-12">
         <p className="company">
           <UscImage />
-          <strong>University of Southern California</strong>
+          <strong>USC</strong>
           <small>2016 - present</small>
         </p>
         <p>
           <strong>Software Developer</strong>
         </p>
         <p>
-          Lead developer for the USC Clinical Trials website redesign using Ruby
-          / Rails / Postgres / ZURB Foundation. Building the workflow enabling
-          researchers to create landing pages for their clinical trials with the
-          intent to increase volunteer recruitment. Key accomplishments include
-          integration of the Facebook/Instagram Ad API, Twilio API, address
-          geocoding, language translation and elastisearch. Software development
-          workflow using Git, Rspec, Github, Heroku and CodeClimate.
+          Lead developer for the University of Southern California (USC)
+          Clinical Trials website redesign using Ruby / Rails / Postgres / ZURB
+          Foundation. Building the workflow enabling researchers to create
+          landing pages for their clinical trials with the intent to increase
+          volunteer recruitment. Key accomplishments include integration of the
+          Facebook/Instagram Ad API, Twilio API, address geocoding, language
+          translation and elastisearch. Software development workflow using Git,
+          Rspec, Github, Heroku and CodeClimate.
         </p>
       </article>
     </div>
